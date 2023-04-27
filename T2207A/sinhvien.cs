47c074ff43394c7dcf25a5e5a683f6588912f17a
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2023-04-27 08:32:22 +0700

    up

--- T2207A/sinhvien.cs
+++ T2207A/sinhvien.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace T2207A
+public class sinhvien
 {
-    internal class sinhvien
+    public int id { get; set; }
+    public string name { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public float mathScore { get; set; }
+    public float physicsScore { get; set; }
+    public float chemistryScore { get; set; }
+    public float averageScore
     {
+        get { return (mathScore + physicsScore + chemistryScore) / 3; }
+    }
+    public string grade
+    {
+        get
+        {
+            if (averageScore >= 8)
+            {
+                return "Giỏi";
+            }
+            else if (averageScore >= 6.5)
+            {
+                return "Khá";
+            }
+            else if (averageScore >= 5)
+            {
+                return "Trung bình";
+            }
+            else
+            {
+                return "Yếu";
+            }
+        }
     }
 }
+
